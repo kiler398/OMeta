e21eb11a72d7863f152dfec39b3aa4c815dee26a
kiler <kiler398@vip.qq.com>
2020-05-13 07:15:18 +0800

    修改提交脚本

--- src/Sonarcube.bat
+++ src/Sonarcube.bat
@@ -1,3 +1,3 @@
-dotnet sonarscanner begin /k:"OMeta" /d:sonar.host.url="http://120.92.169.203:9192" /d:sonar.login="24a6b060dae5d3b5082a3183e3d54c02751fe568"
+dotnet sonarscanner begin /k:"OMeta" /d:sonar.host.url="http://120.92.169.203:9192" /d:sonar.login="cd5769da76122b01818758cd50f6c8947f16f31a"
 dotnet build OMeta.sln
-dotnet sonarscanner end  /d:sonar.login="24a6b060dae5d3b5082a3183e3d54c02751fe568"
+dotnet sonarscanner end  /d:sonar.login="cd5769da76122b01818758cd50f6c8947f16f31a"
